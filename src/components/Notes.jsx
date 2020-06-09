e97cd2f36291ecc0e2b098e562a54fbf3fec64ac
Chad Hinds <chadrickj8@gmail.com>
2020-06-09 15:47:56 -0400

    Add a new item to notes  data

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -24,6 +24,12 @@ const notes = [
         title: "Hardware vs. Software",
         content:
             "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+    },
+    {
+        key: 5,
+        title: "Dev and Nicolle got the yams",
+        content:
+            "I say this respectfully but they are next level and should be kept safe."
     }
 ];
 
