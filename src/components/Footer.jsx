8cbc44fe05c9579ba607036411a56187a0cd61ba
Chad Hinds <chadrickj8@gmail.com>
2020-06-09 15:01:15 -0400

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h4>Copyright {new Date().getFullYear()}</h4>
+        </div>
+    );
+};
+
+export default Footer;
