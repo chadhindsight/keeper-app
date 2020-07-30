84630e3116a184c8437bca5da8aee9b37939f4ab
Chad Hinds <chadrickj8@gmail.com>
2020-07-30 17:54:32 -0400

    setup component state with hooks

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,42 +2,52 @@ import React from "react";
 import { useState } from "react";
 
 function CreateArea(props) {
-    // Use hooks to setup component state
-   const [note, setNote] = useState({
-        title: "",
-        content: ""
-   });
-   function hadnleChange(e){
+  // Use hooks to setup component state!
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function hadnleChange(e) {
     //First, destructure the event object
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
-    setNote(prevNote => {
-        return {
-            ...prevNote,
-            [name]: value
-        }
-    })
-   }
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
 
-    function submitNote(e) {
-        e.preventDefault()
-        props.onAdd(note)
-        setNote({
-            title: "",
-            content: ""
-        })
-    }
+  function submitNote(e) {
+    e.preventDefault();
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+  }
 
-
-    return (
-        <div>
-            <form>
-                <input name="title" onChange={hadnleChange}value={note.title} placeholder="Title" />
-                <textarea name="content" onChange={hadnleChange}value={note.content} placeholder="Take a note..." rows="3" />
-                <button onClick={submitNote}>Add</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={hadnleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={hadnleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
