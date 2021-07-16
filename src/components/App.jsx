29b6edb7df6c4a0252084397c0073fa3fcc09d0a
Chad Hinds <chadrickj8@gmail.com>
2021-07-16 07:40:02 -0400

    Adjust comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import Footer from './Footer.jsx';
 
 class App extends Component {
     state = {
-        notes: [{company: 'dummy', content: 'bummy'}]
+        notes: [{ company: 'dummy', content: 'bummy' }]
     }
 
     // Add a new note to state. Pass down through props
@@ -16,8 +16,8 @@ class App extends Component {
         })
         console.log(this.state)
     }
-    // Remove a specified note! Pass down through props
-    deleteNote = (id) =>{
+    // Remove a specified note! Pass down through props.
+    deleteNote = (id) => {
         this.setState({
             notes: this.state.notes.filter((noteItem, index) => {
                 return index !== id;
@@ -30,15 +30,15 @@ class App extends Component {
             <div>
                 <Header />
                 <CreateArea
-                onAdd={this.addNote} />
+                    onAdd={this.addNote} />
 
-                {this.state.notes.map((noteItem, index)=>{
+                {this.state.notes.map((noteItem, index) => {
                     return <Note
-                     key={index}
-                     id={index}
-                    title={noteItem.title}
-                    content={noteItem.content}
-                    onDelete={this.deleteNote} />
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={this.deleteNote} />
                 })}
                 <Footer />
             </div>
